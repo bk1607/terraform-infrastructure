6842fb0e0751bc5c8866be2df4f5d89897c20189
bk1607 <baluanumala16@gmail.com>
2024-01-04 10:22:57 +0530

    adding output block

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 #local variables
 locals {
   subnet_ids = {
-    db =  data.aws_subnets.vpc_subnets.ids
-    app = data.aws_subnets.app_subnets.ids
-    web = data.aws_subnets.web_subnets.ids
-    public = data.aws_subnets.public_subnets
+    db =  tolist(data.aws_subnets.vpc_subnets.ids)
+    app = tolist(data.aws_subnets.app_subnets.ids)
+    web = tolist(data.aws_subnets.web_subnets.ids)
+    public = tolist(data.aws_subnets.public_subnets)
   }
   db_subnet_ids =  data.aws_subnets.vpc_subnets.ids
   app_subnet_ids = data.aws_subnets.app_subnets.ids
