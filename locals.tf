91868cd862602d741887e80dec19017076c395f8
bk1607 <baluanumala16@gmail.com>
2024-01-07 11:54:48 +0530

    debugging

--- locals.tf
+++ locals.tf
@@ -18,5 +18,9 @@
 locals {
   subnet_ids = {
     db = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].id, module.vpc["main"].private_subnets["db_private_subnet_2"].id])
+    app =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].id, module.vpc["main"].private_subnets["app_private_subnet_2"].id])
+    web = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].id, module.vpc["main"].private_subnets["web_private_subnet_2"].id])
+    public = tolist([module.vpc["main"].public_subnets["public_subnet_1"].id, module.vpc["main"].public-subnets["public_subnet_2"].id])
+
   }
 }
