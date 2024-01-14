675c7e97c75d9d4f0188f99ba151f1311a7fec4b
bk1607 <baluanumala16@gmail.com>
2024-01-14 22:29:56 +0530

    rabbitmq debugging

--- locals.tf
+++ locals.tf
@@ -1,38 +1,23 @@
 #local variables
+
 #locals {
 #  subnet_ids = {
-#    db =  data.aws_subnets.vpc_subnets.ids
-#    app = data.aws_subnets.app_subnets.ids
-#    web = data.aws_subnets.web_subnets.ids
-#    public = data.aws_subnets.public_subnets.ids
+#    db = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].id, module.vpc["main"].private_subnets["db_private_subnet_2"].id])
+#    app =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].id, module.vpc["main"].private_subnets["app_private_subnet_2"].id])
+#    web = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].id, module.vpc["main"].private_subnets["web_private_subnet_2"].id])
+#    public = tolist([module.vpc["main"].public_subnets["public_subnet_1"].id, module.vpc["main"].public_subnets["public_subnet_2"].id])
+#
 #  }
+#  cidr_blocks = {
+#    db = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["db_private_subnet_2"].cidr_block])
+#    app =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["app_private_subnet_2"].cidr_block])
+#    web = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["web_private_subnet_2"].cidr_block])
+#    public = tolist([module.vpc["main"].public_subnets["public_subnet_1"].cidr_block, module.vpc["main"].public_subnets["public_subnet_2"].cidr_block])
 #
-#  db_subnet_ids =  data.aws_subnets.vpc_subnets.ids
-#  app_subnet_ids = data.aws_subnets.app_subnets.ids
-#  web_subnet_ids = data.aws_subnets.web_subnets.ids
-#}
+#  }
+#  db_subnet_ids = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].id, module.vpc["main"].private_subnets["db_private_subnet_2"].id])
+#  app_subnet_ids =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].id, module.vpc["main"].private_subnets["app_private_subnet_2"].id])
+#  web_subnet_ids = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].id, module.vpc["main"].private_subnets["web_private_subnet_2"].id])
+#  public_subnet_ids = tolist([module.vpc["main"].public_subnets["public_subnet_1"].id, module.vpc["main"].public_subnets["public_subnet_2"].id])
 #
-#locals {
-#  subnet_info = { for s in data.aws_subnet.example : s.tags["Name"] => s.cidr_block}
 #}
-locals {
-  subnet_ids = {
-    db = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].id, module.vpc["main"].private_subnets["db_private_subnet_2"].id])
-    app =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].id, module.vpc["main"].private_subnets["app_private_subnet_2"].id])
-    web = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].id, module.vpc["main"].private_subnets["web_private_subnet_2"].id])
-    public = tolist([module.vpc["main"].public_subnets["public_subnet_1"].id, module.vpc["main"].public_subnets["public_subnet_2"].id])
-
-  }
-  cidr_blocks = {
-    db = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["db_private_subnet_2"].cidr_block])
-    app =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["app_private_subnet_2"].cidr_block])
-    web = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].cidr_block, module.vpc["main"].private_subnets["web_private_subnet_2"].cidr_block])
-    public = tolist([module.vpc["main"].public_subnets["public_subnet_1"].cidr_block, module.vpc["main"].public_subnets["public_subnet_2"].cidr_block])
-
-  }
-  db_subnet_ids = tolist([module.vpc["main"].private_subnets["db_private_subnet_1"].id, module.vpc["main"].private_subnets["db_private_subnet_2"].id])
-  app_subnet_ids =  tolist([module.vpc["main"].private_subnets["app_private_subnet_1"].id, module.vpc["main"].private_subnets["app_private_subnet_2"].id])
-  web_subnet_ids = tolist([module.vpc["main"].private_subnets["web_private_subnet_1"].id, module.vpc["main"].private_subnets["web_private_subnet_2"].id])
-  public_subnet_ids = tolist([module.vpc["main"].public_subnets["public_subnet_1"].id, module.vpc["main"].public_subnets["public_subnet_2"].id])
-
-}
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 #output "vpc_details" {
 #  value = local.subnet_ids
 #}
-output "vpc_id" {
-  value = module.vpc["main"].vpc_id
-}
-
-output "alb" {
-  value = module.alb["private"].alb_details["dns_name"]
-}
-output "listener_arn" {
-  value = module.alb["private"].listener_details
-}
+#output "vpc_id" {
+#  value = module.vpc["main"].vpc_id
+#}
+#
+#output "alb" {
+#  value = module.alb["private"].alb_details["dns_name"]
+#}
+#output "listener_arn" {
+#  value = module.alb["private"].listener_details
+#}
